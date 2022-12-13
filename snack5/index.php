c160ddc9fddd46b787342a3f0ef183d170ed2c2f
cosmo-18 <cosimo.simonesolfrizzi@gmail.com>
2022-12-14 00:47:26 +0100

    5 completo

--- snack5/index.php
+++ snack5/index.php
@@ -1,21 +1,11 @@
-<!-- Snack 4
-Creare un array con 15 numeri casuali, tenendo conto che l’array non dovrà contenere lo stesso numero più di una volta -->
+<!-- Snack 5
+Prendere un paragrafo abbastanza lungo, contenente diverse frasi. Prendere il paragrafo e suddividerlo in tanti paragrafi. Ogni punto un nuovo paragrafo. -->
 
 <?php
 
-$numeroArray = [];
+$frase = 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sit ut ad, optio quod ea eveniet quidem! Magnam enim dolor sapiente ea ratione quis in, vitae quas provident, numquam tempore est. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Veritatis hic quam ipsam pariatur consectetur, rerum quod at rem omnis accusamus ea possimus beatae voluptas quisquam adipisci, veniam modi! At, error? Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi doloribus vitae ipsam, nobis voluptas itaque est deserunt! Ratione, quae! Modi eaque voluptate nostrum. Provident assumenda incidunt delectus nihil magni reprehenderit? Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi animi odit omnis voluptatibus sit quidem fugit explicabo voluptate doloremque totam adipisci tempore, eligendi quisquam molestias quod natus necessitatibus enim optio.';
 
-$soldier = 0;
-
-for($i=0; $i < 16; $i++){
-   $num = rand(1, 100);
-   if($num != $soldier){
-    array_push($numeroArray, $num);
-    $soldier == $num;
-   }else{
-    $i--;
-   }
-}
+$pezzo=  explode('.', $frase);
 
 ?>
 
@@ -25,7 +15,7 @@ for($i=0; $i < 16; $i++){
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>SNACK_4</title>
+    <title>SNACK_5</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.2/font/bootstrap-icons.css">
     <link href='https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css' rel='stylesheet' integrity='sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi' crossorigin='anonymous'>
     <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css' integrity='sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==' crossorigin='anonymous' referrerpolicy='no-referrer'/>
@@ -35,10 +25,8 @@ for($i=0; $i < 16; $i++){
 
 <?php
 
-
-for($i = 0; $i < count($numeroArray); $i++){
-    echo $numeroArray[$i];
-    echo '<br/>';
+forEach($pezzo as $punto){
+    echo $punto;
     echo '<br/>';
 }
 
